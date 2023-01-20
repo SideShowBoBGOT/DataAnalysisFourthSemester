fed5c7340cb73220d8fcd9ab17833cf32f958033
sideshowbobgot <sideshowbobgot@gmail.com>
2023-01-20 21:31:09 +0200

    added report

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,12 +1,8 @@
-import pandas as pd
-
-from my_etl_controller import get_main_engine, get_stage_engine, get_session, transform_data
-from my_models import main_models
-
+from my_etl_controller import get_main_engine, get_stage_engine, transform_data
 
 def main():
-    stage_engine = get_stage_engine(False)
-    main_engine = get_main_engine(False)
+    stage_engine = get_stage_engine(True)
+    main_engine = get_main_engine(True)
     transform_data(stage_engine, main_engine)
 
 if __name__ == '__main__':

b7af4fe798b8e323890125a698b74af08d4e7885
sideshowbobgot <sideshowbobgot@gmail.com>
2023-03-02 01:02:04 +0200

    lab3: almost done

--- Lab3/main.py
+++ Lab3/main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from my_colors import bcolors
+
 
 def read_dataset(path: str, sep: str = ';') -> pd.DataFrame:
     data = pd.read_csv(path, sep=sep)
@@ -46,33 +48,61 @@ if __name__ == "__main__":
     df_max_gdp = dataset.nlargest(1, ['GDP per capita'])
     max_gdp = df_max_gdp['GDP per capita'].values[0]
     country_max_gdp = df_max_gdp['Country Name'].values[0]
-    print(f'Max GDP:\n\tCountry: {country_max_gdp}\t\tGDP: {max_gdp}')
+    print(f'{bcolors.HEADER}Max GDP:\n{bcolors.ENDC}'
+          f'\t{bcolors.OKBLUE}Country: '
+          f'{bcolors.OKGREEN}{country_max_gdp}\t'
+          f'\t{bcolors.OKBLUE}GDP: {bcolors.OKGREEN}{max_gdp}')
 
     df_min_area = dataset.nsmallest(1, ['Area'])
     min_area = df_min_area['Area'].values[0]
     country_min_area = df_min_area['Country Name'].values[0]
-    print(f'Min Area:\n\tCountry: {country_min_area}\t\tArea: {min_area}')
+    print(f'{bcolors.HEADER}Min Area:\n{bcolors.ENDC}'
+          f'\t{bcolors.OKBLUE}Country: '
+          f'{bcolors.OKGREEN}{country_min_area}\t'
+          f'\t{bcolors.OKBLUE}Area: '
+          f'{bcolors.OKGREEN}{min_area}')
 
     # 3 В якому регіоні середня площа країни найбільша?
     df_group_by_region = dataset.groupby('Region')
-    df_region_area_sum = df_group_by_region.sum()[['Area']]
+    df_region_area_sum = df_group_by_region.sum(numeric_only=True)[['Area']]
     df_region_count = df_group_by_region.count()[['Area']]
     df_average_area = df_region_area_sum / df_region_count
     df_max_average_area = df_average_area.nlargest(1, ['Area'])
     max_average_area_region = df_max_average_area.axes[0][0]
     max_average_area = df_max_average_area['Area'].values[0]
-    print(f'Max average area:\n\tRegion: {max_average_area_region}\t\tValue: {max_average_area}')
+    print(f'{bcolors.HEADER}Max average area:{bcolors.ENDC}\n'
+          f'\t{bcolors.OKBLUE}Region: '
+          f'{bcolors.OKGREEN}{max_average_area_region}\t'
+          f'\t{bcolors.OKBLUE}Value: '
+          f'{bcolors.OKGREEN}{max_average_area}')
 
     # 5 Чи співпадає в якомусь регіоні середнє та медіана ВВП?
     df_region_gdp_mean = pd.DataFrame()
     df_region_gdp_average = pd.DataFrame()
-    df_region_gdp_mean['Mean'] = df_group_by_region.mean()[['GDP per capita']]
-    df_region_gdp_average['Average'] = df_group_by_region.median()[['GDP per capita']]
+    df_region_gdp_mean['Mean'] = df_group_by_region.mean(numeric_only=True)[['GDP per capita']]
+    df_region_gdp_average['Average'] = df_group_by_region.median(numeric_only=True)[['GDP per capita']]
     df_region_mean_average = pd.concat([df_region_gdp_mean, df_region_gdp_average], axis=1)
-    print(df_region_mean_average)
-    #df_region_any_mean_median_equal = df_group_by_region.
+    df_region_mean_average['Difference'] = df_region_mean_average['Mean'] - df_region_mean_average['Average']
+    df_region_mean_average['Difference'] = df_region_mean_average['Difference'].abs()
+    df_smallest_mean_average_difference = df_region_mean_average.nsmallest(1, ['Difference'])
+    mean_average_info = bcolors.HEADER + 'Mean-Average Equality:\n' + bcolors.ENDC
+    mean_average_info += f'\t{bcolors.OKBLUE}Region: {bcolors.OKGREEN}{df_smallest_mean_average_difference.axes[0][0]}'
+    for column_name in ['Mean', 'Average', 'Difference']:
+        mean_average_info += f'\t\t{bcolors.OKBLUE}{column_name}: ' \
+                             f'{bcolors.OKGREEN}' \
+                             f'{df_smallest_mean_average_difference[column_name].values[0]}' \
+                             f'{bcolors.ENDC}'
+    print(mean_average_info)
 
-    print(dataset.columns)
+    # 6 Вивести топ 5 країн та 5 останніх країн по ВВП та кількості СО2 на душу населення.
+    dataset_gdp_desc = dataset.sort_values('GDP per capita', ascending=False)
+    dataset_gdp_asc = dataset.sort_values('GDP per capita', ascending=True)
+    dataset_co2_desc = dataset.sort_values('CO2 emission', ascending=False)
+    dataset_co2_asc = dataset.sort_values('CO2 emission', ascending=True)
+    print(f'{bcolors.HEADER}GDP Top 5:{bcolors.ENDC}\n', dataset_gdp_desc.head(5).to_string())
+    print(f'{bcolors.HEADER}GDP Bottom 5:{bcolors.ENDC}\n', dataset_gdp_asc.head(5).to_string())
+    print(f'{bcolors.HEADER}CO2 Top 5:{bcolors.ENDC}\n', dataset_co2_desc.head(5).to_string())
+    print(f'{bcolors.HEADER}CO2 Bottom 5:{bcolors.ENDC}\n', dataset_co2_asc.head(5).to_string())
 
 
 
